382af87f2f3e5210dd5bda7c608b7490d310a6ff
ElijahInamarga <elijahcek@gmail.com>
2025-12-31 22:07:16 -0800

    working localhost chatroom

--- client/client.c
+++ client/client.c
@@ -1,13 +1,14 @@
 #include <netinet/in.h>
+#include <poll.h>
 #include <stdio.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
 
+#define BUFFER_SIZE 256
+
 int main()
 {
-    char server_response[256];
-
     int socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
     if(socket_fd == -1) {
         printf("Error creating socket\n");
@@ -15,10 +16,7 @@ int main()
     }
 
     // destination address
-    struct sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(9002);
-    server_addr.sin_addr.s_addr = INADDR_ANY;
+    struct sockaddr_in server_addr = {AF_INET, htons(9999), 0};
 
     int connect_status = connect(socket_fd, (struct sockaddr *)&server_addr,
                                  sizeof(server_addr));
@@ -27,9 +25,28 @@ int main()
         return -1;
     }
 
-    recv(socket_fd, server_response, sizeof(server_response), 0);
-    printf("Server response: %s\n", server_response);
+    struct pollfd fds[2] = {{0, POLLIN, 0}, {socket_fd, POLLIN, 0}};
 
+    // 0000 0000 0000 0001 == data available
+    for(;;) {
+        char buffer[256] = {0};
+
+        poll(fds, 2, 50000);
+
+        // read stdin and send to server
+        if(fds[0].revents & POLLIN) {
+            read(0, buffer, 255);
+            send(socket_fd, buffer, BUFFER_SIZE - 1, 0);
+        }
+
+        // read server input and print to stdout
+        if(fds[1].revents & POLLIN) {
+            if(recv(socket_fd, buffer, BUFFER_SIZE - 1, 0) == 0) {
+                return 0;
+            }
+            printf("%s\n", buffer);
+        }
+    }
     close(socket_fd);
 
     return 0;
--- server/server.c
+++ server/server.c
@@ -1,37 +1,57 @@
 #include <netinet/in.h>
+#include <poll.h>
 #include <stdio.h>
+#include <sys/poll.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
 
+#define BUFFER_SIZE 256
+
 int main()
 {
-    char message[256] = "Successfully connected";
-
-    int socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
-    if(socket_fd == -1) {
-        printf("Error creating socket\n");
-	return -1;
+    int passive_socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+    if(passive_socket_fd == -1) {
+        printf("Error creating server socket\n");
+        return -1;
     }
 
     // server address
-    struct sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(9002);
-    server_addr.sin_addr.s_addr = INADDR_ANY;
+    struct sockaddr_in server_addr = {AF_INET, htons(9999), 0};
 
-    int bind_status = bind(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    int bind_status = bind(passive_socket_fd, (struct sockaddr *)&server_addr,
+                           sizeof(server_addr));
     if(bind_status != 0) {
-	printf("Error binding server socket to IP and port\n");
-	return -1;
+        printf("Error binding server socket to IP and port\n");
+        return -1;
     }
 
-    listen(socket_fd, 1);
+    listen(passive_socket_fd, 10);
 
-    int client_socket_fd = accept(socket_fd, NULL, NULL);
+    int active_socket_fd = accept(passive_socket_fd, NULL, NULL);
 
-    send(client_socket_fd, message, sizeof(message), 0);
+    struct pollfd fds[2] = {{0, POLLIN, 0}, {active_socket_fd, POLLIN, 0}};
 
-    close(socket_fd);
+    // 0000 0000 0000 0001 == data available
+    for(;;) {
+        char buffer[BUFFER_SIZE] = {0};
+        poll(fds, 2, 50000);
+
+        // read stdin and send to client
+        if(fds[0].revents & POLLIN) {
+            read(0, buffer, 255);
+            send(active_socket_fd, buffer, BUFFER_SIZE - 1, 0);
+        }
+
+        // read client socket and print to stdout
+        if(fds[1].revents & POLLIN) {
+            if(recv(active_socket_fd, buffer, BUFFER_SIZE - 1, 0) == 0) {
+                return 0;
+            }
+            printf("%s\n", buffer);
+        }
+    }
+
+    close(passive_socket_fd);
     return 0;
 }
