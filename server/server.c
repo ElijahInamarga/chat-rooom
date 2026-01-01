6c66c36a409e7739e682a12605f005cac605b535
ElijahInamarga <elijahcek@gmail.com>
2026-01-01 13:27:21 -0800

    split server code to 5 steps

--- server/server.c
+++ server/server.c
@@ -49,17 +49,16 @@ int start_session(int socketfd)
     return 0;
 }
 
-void close_session(int socketfd, int passive_socketfd)
+void close_session(int socketfd)
 {
     close(socketfd);
-    close(passive_socketfd);
 }
 
-int main()
+int start_server()
 {
-    int passive_socketfd;
-    passive_socketfd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
-    if(passive_socketfd == -1) {
+    int socketfd;
+    socketfd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+    if(socketfd == -1) {
         printf("Error creating passive socket\n");
         return -1;
     }
@@ -70,27 +69,47 @@ int main()
     server_addr.sin_port = htons(PORT_NUM);
     server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    int bind_status = bind(passive_socketfd, (struct sockaddr *)&server_addr,
-                           sizeof(server_addr));
+    int bind_status =
+        bind(socketfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
 
     if(bind_status == -1) {
         printf("Error binding passive socket to IP and port\n");
         return -1;
     }
 
-    listen(passive_socketfd, BACKLOG_LEN);
+    listen(socketfd, BACKLOG_LEN);
     printf("Server start\n\n");
+    return socketfd;
+}
 
-    int socketfd;
-    socketfd = accept(passive_socketfd, NULL, NULL);
+void close_server(int passive_socketfd)
+{
+    close(passive_socketfd);
+}
+
+int main()
+{
+    // start server
+    int passive_socketfd = start_server();
+    if(passive_socketfd == -1) {
+        printf("Error starting server\n");
+        return -1;
+    }
+
+    // wait for clients
+    int socketfd = accept(passive_socketfd, NULL, NULL);
     if(socketfd == -1) {
         printf("Error accepting connection\n");
         return -1;
     }
-
     printf("Client has connected\n");
+
+    // begin session with client
     int session_result = start_session(socketfd);
 
-    close_session(socketfd, passive_socketfd);
+    close_session(socketfd);
+
+    close_server(passive_socketfd);
+
     return session_result;
 }
