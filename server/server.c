539ee37f4b4d9d1dc8d003a90e325c93d45f29ee
ElijahInamarga <elijahcek@gmail.com>
2026-01-01 12:53:26 -0800

    modularize server code

--- server/server.c
+++ server/server.c
@@ -11,40 +11,8 @@
 #define BACKLOG_LEN 3
 #define NUM_FDS     2
 
-int main()
+int start_session(int socketfd)
 {
-    int passive_socketfd;
-    passive_socketfd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
-    if(passive_socketfd == -1) {
-        printf("Error creating passive socket\n");
-        return -1;
-    }
-
-    // server info
-    struct sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT_NUM);
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-
-    int bind_status = bind(passive_socketfd, (struct sockaddr *)&server_addr,
-                           sizeof(server_addr));
-
-    if(bind_status == -1) {
-        printf("Error binding passive socket to IP and port\n");
-        return -1;
-    }
-
-    listen(passive_socketfd, BACKLOG_LEN);
-
-    int socketfd;
-    socketfd = accept(passive_socketfd, NULL, NULL);
-    if(socketfd == -1) {
-        printf("Error accepting connection\n");
-        return -1;
-    }
-        
-    printf("Client has connected\n");
-
     // stdin and socket
     struct pollfd fds[NUM_FDS] = {{0, POLLIN, 0}, {socketfd, POLLIN, 0}};
 
@@ -78,8 +46,51 @@ int main()
         }
     }
 
+    return 0;
+}
+
+void close_session(int socketfd, int passive_socketfd)
+{
     close(socketfd);
     close(passive_socketfd);
+}
 
-    return 0;
+int main()
+{
+    int passive_socketfd;
+    passive_socketfd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+    if(passive_socketfd == -1) {
+        printf("Error creating passive socket\n");
+        return -1;
+    }
+
+    // server info
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT_NUM);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    int bind_status = bind(passive_socketfd, (struct sockaddr *)&server_addr,
+                           sizeof(server_addr));
+
+    if(bind_status == -1) {
+        printf("Error binding passive socket to IP and port\n");
+        return -1;
+    }
+
+    listen(passive_socketfd, BACKLOG_LEN);
+    printf("Server start\n\n");
+
+    int socketfd;
+    socketfd = accept(passive_socketfd, NULL, NULL);
+    if(socketfd == -1) {
+        printf("Error accepting connection\n");
+        return -1;
+    }
+
+    printf("Client has connected\n");
+    int session_result = start_session(socketfd);
+
+    close_session(socketfd, passive_socketfd);
+    return session_result;
 }
